32c085d8ad9caba6115c95fdcc1cc629950bd989
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-08-16 21:24:16 +0530

    updated

--- src/components/Products.js
+++ src/components/Products.js
@@ -11,10 +11,37 @@ const ProductItem = styled.div`
   margin: 20px 0;
 `;
 
+const styles = {
+  container: {
+    display: "inline-block",
+    background: "linear-gradient(to right, #e0eafc, #cfdef3)",
+    padding: "5px 10px",
+    borderRadius: "5px",
+  },
+  label: {
+    color: "#003366",
+    fontSize: "18px",
+    fontWeight: "bold",
+    transform: "skewX(-10deg)",
+    display: "inline-block",
+  },
+};
+
+
 const Products = () => {
   return (
     <ProductsContainer>
-      <h2>Cutting-edge hardware</h2>
+      <div style={styles.container}>
+        <span style={styles.label}>Products</span>
+      </div>
+      <h2
+        style={{
+          width: "550px",
+        }}
+      >
+        <b>Cutting-edge hardware</b>, offerings helping cities optimize
+        resources and achieve smart development goals
+      </h2>
       <ProductItem>
         <h3>Centralized Control & Monitoring System</h3>
         <p>Description of the system...</p>
